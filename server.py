1f2e9649c16bddc972c827e1d5a15e068ecebac9
3n3s43 <aksacli1643@gmail.com>
2026-03-04 23:21:49 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 import os
-from flask import Flask, send_from_directory, jsonify
+from flask import Flask, send_from_directory
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
 CORS(app)
-# Render ve benzeri platformlarda kopma olmaması için transports ve async_mode ekledik
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
+
+# Gunicorn + Eventlet ikilisi için en stabil ayar budur
+socketio = SocketIO(app, 
+    cors_allowed_origins="*", 
+    async_mode='eventlet',
+    logger=True, 
+    engineio_logger=True)
 
 @app.route('/')
 def index(): 
@@ -20,23 +26,17 @@ def remote_page():
 def static_files(path): 
     return send_from_directory('.', path)
 
-# 1. ASLAN VE OTURTMA SİNYALİ (Kritik: HTML'deki isimle aynı olmalı)
 @socketio.on('execute_visual')
 def handle_visual(data):
-    # Remote'dan gelen veriyi (manage_slot veya lion_trigger) olduğu gibi Host'a basar
     emit('execute_visual', data, broadcast=True)
 
-# 2. DÜZENLEME (EDIT MOVE) SİNYALİ
 @socketio.on('toggle_edit')
 def handle_edit(data):
     emit('toggle_edit', data, broadcast=True)
 
-# Hata ayıklama için terminale yazdırır
-@socketio.on('connect')
-def test_connect():
-    print(">>> Bir cihaz sisteme bağlandı!")
-
 if __name__ == '__main__':
-    # Render veya yerel çalıştırma için port ayarı
-    port = int(os.environ.get("PORT", 5001))
+    # Render/Orender için portu dışarıdan alıyoruz
+    port = int(os.environ.get("PORT", 10000))
+    # Gunicorn kullanırken bu kısım aslında devre dışı kalır ama 
+    # yerelde test için durması iyidir.
     socketio.run(app, host='0.0.0.0', port=port)
